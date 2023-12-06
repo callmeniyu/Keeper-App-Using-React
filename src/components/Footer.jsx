7805d78ed8f460fbbd722667c8e475945a7e5b56
callmeniyu <getniyashere@gmail.com>
2023-12-06 17:36:15 +0530

    FInal product

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+
+function Footer() {
+    const today = new Date();
+    const year = today.getFullYear();
+
+    return (
+        <footer><p>Copyright@{year}</p></footer>
+    )
+}
+
+export default Footer;
